a47f388172c274666b9d52931cfab001145ec5f7
Rikovald <rikonvald@gmail.com>
2024-10-10 15:49:01 +0300

    toRem and mixin standartiz

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -7,32 +7,34 @@ import inst from 'shared/assets/social/Instagram_white.svg';
 export const Footer: FC = (): ReactElement => {
     return (
         <div className={s.root}>
-            © 2022 All rights reserved
-            <div className={s.social}>
-                <a
-                    className={s.href}
-                    href="https://www.tiktok.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <img src={tt} alt="tiktok" className={s.icon} />
-                </a>
-                <a
-                    className={s.href}
-                    href="https://vk.com/"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <img src={vk} alt="vk" className={s.icon} />
-                </a>
-                <a
-                    className={s.href}
-                    href="https://www.instagram.com"
-                    target="_blank"
-                    rel="noreferrer"
-                >
-                    <img src={inst} alt="inst" className={s.icon} />
-                </a>
+            <div className={s.wrapper}>
+                © 2022 All rights reserved
+                <div className={s.social}>
+                    <a
+                        className={s.href}
+                        href="https://www.tiktok.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={tt} alt="tiktok" className={s.icon} />
+                    </a>
+                    <a
+                        className={s.href}
+                        href="https://vk.com/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={vk} alt="vk" className={s.icon} />
+                    </a>
+                    <a
+                        className={s.href}
+                        href="https://www.instagram.com"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <img src={inst} alt="inst" className={s.icon} />
+                    </a>
+                </div>
             </div>
         </div>
     );
